170b77cb93d8cc350e6fe003ef06e40f4cca3e1b
Raymond Cox <raymondcdev@gmail.com>
2025-01-25 20:14:35 -0500

    Fix for navigation

--- src/components/molecules/Nav/Nav.tsx
+++ src/components/molecules/Nav/Nav.tsx
@@ -1,6 +1,14 @@
 import { Typography } from '../../atoms'
 import * as classes from './Nav.module.css'
 
+/**
+ * The prefix for the path. Deployed to GitHub Pages, the prefix is '/poe2-tools'.
+ */
+const prefix = process.env.NODE_ENV === 'development' ? '' : '/poe2-tools'
+
+/**
+ * The links in the navigation.
+ */
 const links = [{ href: '/', label: 'DPS Calculator' }]
 
 const Nav = (): React.JSX.Element => {
@@ -11,7 +19,7 @@ const Nav = (): React.JSX.Element => {
       <hr />
       {links.map((link) => {
         const className =
-          link.href === window.location.pathname ? classes.active : ''
+          link.href === prefix + window.location.pathname ? classes.active : ''
 
         return (
           <a key={link.href} href={link.href} className={className}>
